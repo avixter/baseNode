8b0f3cf0769f3884e099caaeeb88e30ca68375eb
avixter <avixter@users.noreply.github.com>
2017-10-05 00:46:31 +0200

    added server;

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-var a;
+var express = require('express');
+
+var app = express();
+
+app.listen(3000, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Running on port 3000");
+    }
+});
